bca0ae7733a237a3fc05bfe374c5e5f87fb2836a
hayu6 <hayu6944@gmail.com>
2024-11-08 14:38:05 +0900

    1108

---
+++ src/main/java/com/exampl/traveler/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.exampl.traveler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private String resourcePath = "/upload/**"; // view에서 접근할 경로
+    private String savePath = "file:///C:/springboot_img/"; // 실제 파일 저장 경로
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcePath)
+                .addResourceLocations(savePath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("https://traveler-a6a9g9g5ggf7aab4.koreacentral-01.azurewebsites.net") // Azure 배포 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 필요한 HTTP 메서드 허용
+    }
+}
